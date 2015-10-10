ca87ccd37efce1445ad3eeecf8e20e47affd3b48
emmanuel velez <emmanuelhcpk@MacBook-Pro-de-emmanuel.local>
2015-10-09 20:29:49 -0500

    con modelo usuario y simulando devovler un array en json en ruta /

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,17 @@
 package controllers
 
 import play.api.mvc._
+import play.api.libs.json.Json
 
+
+import models.User
 object Application extends Controller {
 
+  var listaInt = List(3, 4, 2, 7)
+  var us = new User("cc",1,"Holita","",listaInt,true);
+
   def home= Action {
-    Ok(views.html.home("FLAME's API"))
+    Ok(Json.toJson(us.products))
   }
+
 }
---
+++ app/models/User.scala
@@ -0,0 +1,16 @@
+package models
+
+case class User(
+                 documentType: String,
+                 documentNumber: Int,
+                 firstName: String,
+                 lastName: String,
+                 products: List[Int],
+                 active: Boolean)
+
+object JsonFormats {
+  import play.api.libs.json.Json
+
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+  implicit val userFormat = Json.format[User]
+}
