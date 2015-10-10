da31598e9158f81529e464ab7fff06a2b2f3eadf
emmanuel velez <emmanuelhcpk@MacBook-Pro-de-emmanuel.local>
2015-10-09 21:56:36 -0500

    prueba de find

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,10 +9,26 @@ object Application extends Controller {
 
   var listaInt = List(3, 4, 2, 7)
   var us = new User("cc",1,"Holita","",listaInt,true);
+  var us2 = new User("cc",2,"prueba","",listaInt,true);
+  var us3 = new User("cc",3,"prueba2","",listaInt,true);
+  var usuarios= List(us,us2,us3);
 
   def home= Action {
     Ok(Json.toJson(us.products))
   }
+  def find(document: Int, id: Int)=
+  {
+    var us
+    def comparador(x: User) = {
+      if (x.documentType == document && x.documentNumber == id)
+        {
+          us = x;
+        }
+    }
+    usuarios.map(user => comparador(user))
+    return us;
+  }
+
 
 //  Return User's products based on id and types of document
   def productsUser(document: Int, id: Int) = Action {
