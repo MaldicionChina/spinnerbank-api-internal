3862a7e2b3ad38381fa01310a47cf4d8e39d3104
emmanuel <evelez@insite.com.co>
2015-10-15 20:51:18 -0500

    con clase product

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,47 +2,53 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.json.Json
-
+//import play.api.libs.ws.WS
+//import scala.concurrent.Future
 
 import models.User
 
 object Application extends Controller {
 
-  var listaInt = List("creditos", "CDT","Hipoteca", "Datadebito")
-  var us = new User(1,1,"Holita","",listaInt,true);
-  var us2 = new User(1,2,"prueba","",listaInt,true);
-  var us3 = new User(1,3,"prueba2","",listaInt,true);
-  var usuarios= List(us,us2,us3);
+  var listaInt = List("creditos", "CDT", "Hipoteca", "Datadebito")
+  var us = new User(1, 1, "Holita", "", listaInt, true);
+  var us2 = new User(1, 2, "prueba", "", listaInt, true);
+  var us3 = new User(1, 3, "prueba2", "", listaInt, true);
+  var usuarios = List(us, us2, us3);
 
 
-//  Datos de prueba para detalle de producto
+  //  Datos de prueba para detalle de producto
   var productName = "credito";
   var listDetail = "[{\"id\":1,\"name\":\"CDT\",\"productType\":\"Preferencial\",\"createdAt\":" +
     "\"2015-10-14T21:25:41.456Z\",\"updatedAt\":\"2015-10-14T21:25:41.456Z\",\"CustomerProduct\":" +
     "{\"balance\":\"100.00\",\"createdAt\":\"2015-10-14T21:25:41.525Z\",\"updatedAt\":\"2015-10-14T21:25:41.525Z\"," +
     "\"ProductId\":1,\"CustomerId\":1}}]"
 
-  def home= Action {
+  def home = Action {
     Ok(Json.toJson(us.products))
   }
 
-//  Return User's products based on id and types of document
+  //  Return User's products based on id and types of document
   def productsUser(document: Int, id: Int) = Action {
     // Query a la base de datos SQL
 
-   //List of products
-  val pr = usuarios.filter(x => (x.documentType ==document && x.documentNumber == id))
+    //List of products
+    val pr = usuarios.filter(x => (x.documentType == document && x.documentNumber == id))
 
-   // Ok(views.html.home("Tipo Documento: "+pr.head.documentType+ "Id: "+pr.head.firstName))
-     Ok(Json.toJson(pr.head.products))
+    // Ok(views.html.home("Tipo Documento: "+pr.head.documentType+ "Id: "+pr.head.firstName))
+    Ok(Json.toJson(pr.head.products))
   }
 
 
-  def detailProductUser(id: Int,document: Int, productId: Int) = Action {
+  def detailProductUser(id: Int, document: Int, productId: Int) = Action {
     // Consumo del API del back-end-legacy
     // EJ de URL: https://spinnerbank-api-legacy.herokuapp.com/api/v1/customers/1/products
 
-     Ok(Json.toJson(listDetail))
+    Ok(Json.toJson(listDetail))
   }
+
+
+
+
+
 
 }
---
+++ app/models/Product.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+ * Created by emmanuelhcpk on 15/10/15.
+ */
+class Product {
+
+}
