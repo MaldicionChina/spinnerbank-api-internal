bf47a6badd457e9f81ef7a2d83b1b7f291e1d51f
fersavi <you@example.com>
2015-11-03 19:22:24 -0500

    Se agreja objeto Json

--- app/models/Movement.scala
+++ app/models/Movement.scala
@@ -1,23 +1,30 @@
 package models
 
 import play.api.libs.json.Json
+import org.joda.time.DateTime
+
+
 /**
  * Created by alexis on 16/10/15.
  */
-case class Movement(
-                id: Int,
-                dateMovement: String,
-                valueMovement: Double,
-                description: String
-                ) {
 
-  def toJson() = Json.obj(
+
+case class Movement (id: Int,valueMovement: Double, description: String, dateMovement:DateTime){
+
+ def toJson() = Json.obj(
     "dateMovement" -> this.dateMovement,
     "value" -> this.valueMovement,
     "description" -> this.description
 
   )
+}
+object JsonFormats{
+    
+    implicit val MovementFormat = Json.format[Movement]
+}
+
+
   
 
-}
+
 
